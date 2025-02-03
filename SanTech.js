67c7ab8b7c2c24718ce1fa792c0a6ae2913122e6
ChuBec <vladimirchubec@gmail.com>
2025-02-03 05:29:22 +0200

    Firs commst

---
+++ SanTech.js
@@ -0,0 +1,4 @@
+// Обробник кнопки "Замовити послугу"
+document.getElementById('orderButton').addEventListener('click', function() {
+    alert('Дякуємо за ваше замовлення! Ми зв\'яжемося з вами найближчим часом.');
+});
